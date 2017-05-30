26808d1e43f38a7b000e05c455961d310f56555b
jagrut08 <jagrut_dave@hotmail.com>
2017-05-30 09:06:23 -0400

    Update 2.1.19.cpp

--- LC1/2.1.19.cpp
+++ LC1/2.1.19.cpp
@@ -4,7 +4,48 @@
 #include <stdexcept>
 #include <algorithm>
 
-// O(2^N) time and space
+// BF. Time = O(2^N), space = O(2^N)
+bool populateNumber(const int x, const int idx, const size_t num, size_t &row, std::vector<std::vector<int>>& res) {
+	int cnt = 0;
+	for(; cnt < x && row < res.size(); ++cnt, ++row) {
+			res[row][idx] = num;
+	}
+	return row < res.size()? true : false;
+}
+
+void populateBits(const int x, const int idx, std::vector<std::vector<int>>& res) {
+	size_t row = 0;
+	while(true) {
+		if(!populateNumber(x, idx, 0, row, res)) {
+			return;
+		}
+		if(!populateNumber(x, idx, 1, row, res)) {
+			return;
+		}
+		if(!populateNumber(x, idx, 1, row, res)) {
+			return;
+		}
+		if(!populateNumber(x, idx, 0, row, res)) {
+			return;
+		}
+	}
+}
+
+void printGrayCode(const int numBits) {
+	std::vector<std::vector<int>> temp(1 << numBits, std::vector<int>(numBits, 0));
+	int numOnesZeroes = 1;
+	for(int col = temp[0].size() - 1; col >= 0; --col, numOnesZeroes <<= 1) {
+		populateBits(numOnesZeroes, col, temp);
+	}
+	for(const auto& row : temp) {
+		for(const auto& colVal : row) {
+			std::cout << colVal << " ";
+		}
+		std::cout << '\n';
+	}
+}
+
+// O(2^N) time and space (space is 2^N + N for recursive calls)
 void printLastNBits(const std::vector<int>& v, const int n) {
 	std::ostringstream op;
 	for(const auto& val : v) {
@@ -21,7 +62,7 @@ void printLastNBits(const std::vector<int>& v, const int n) {
 	std::cout << op.str();
 }
 
-std::vector<int> grayCodeOpt(const int n) {
+std::vector<int> printgrayCodeOpt(const int n) {
 	if(n <= 0) {
 		throw std::runtime_error("Invalid input");
 	}
@@ -43,6 +84,8 @@ std::vector<int> grayCodeOpt(const int n) {
 
 int main() {
 	const int n = 4;
-	const auto& res = grayCodeOpt(n);
+//	const auto& res = printGrayCode(n);
+	const auto& res = printGrayCodeOpt(n);
+
 	printLastNBits(res, n);
 }
