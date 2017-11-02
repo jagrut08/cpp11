372f938f8e44418f3113d50deed05df32538163d
jagrut08 <jagrut_dave@hotmail.com>
2017-11-02 17:43:06 -0400

    Update 6.1.cpp

--- LC1/6.1.cpp
+++ LC1/6.1.cpp
@@ -1,62 +1,77 @@
-void mergeSorted(std::vector<int>& a, const std::vector<int>& b)
---
-{1, 5, 10}, {2, 6} => {1, 2, 5, 6, 10}
-{2, 6}, {1, 5, 10} => ||
-{}, {2, 6} => {2, 6}
-{2, 6}, {} => {2, 6}
-{1}, {2} => {1, 2}
-{2}, {1} => {1, 2}
-{2}, {2} => {2, 2}
-{1, 5}, {100, 200} => {1, 5, 100, 200}
-{100, 200}, {1, 5} => {1, 5, 100, 200}
-{1, 100}, {10, 20} => {1, 10, 20, 100}
-{10, 20}, {1, 100} => ||
---
-BF
-	create new vector buf of size a.size()
-	maintain index ai and bi
-	keep adding smaller of ai and bi to buf
-	copy buf over to a
---
-O(m + n) time, O(m + n) space
---
-Opt O(m + n) time, O(1) space
+#include <iostream>
+#include <vector>
 
 void mergeSortedArrays(std::vector<int>& a, const std::vector<int>& b) {
-	int ai = a.size() - 1, bi = b.size() - 1, dst = a.capacity() - 1;
-	
+
+	int ai = a.size() - 1; // Get the last location of a
+	a.resize(a.size() + b.size());  // Now make a bigger so that it can accommodate b
+
+	int bi = b.size() - 1, dst = a.size() - 1; // Get the last location of b and expanded a.
+
+//	std::cout << "Indexes: " << ai << " " << bi << " " << dst << '\n';
 	while(ai >= 0 && bi >= 0) {
 		a[dst--] = a[ai] > b[bi] ? a[ai--] : b[bi--];
 	}
-	 
+
 	while(bi >= 0) {
 		a[dst--] = b[bi--];
 	}
 }
 
 void printArray(const std::vector<int>& v) {
+	if(v.empty()) {
+		std::cout << "{}\n";
+		return;
+	}
+
 	for(const auto& val : v) {
 		std::cout << val << " ";
 	}
-	std::cout << '\n';	
+	std::cout << '\n';
 }
 
 int main() {
-	const std::vector<vector<int>> as = {
+	const std::vector<std::vector<int>> as = {
 		{1, 5, 10},
+				{2, 6},
+				{},
+				{2, 6},
+				{1},
+				{2},
+				{2},
+				{100, 200},
+				{1, 5},
+				{1, 100},
+				{10, 20},
+
+
 	};
-	
-	const std::vector<vector<int>> bs = {
+
+	const std::vector<std::vector<int>> bs = {
 		{2, 6},
+			{1, 5, 10},
+	{2, 6},
+	{},
+	{2},
+	{1},
+	{2},
+	{1, 5},
+	{100, 200},
+	{10, 20},
+	{1, 100},
+
+
 	};
-	
+
 	for(size_t i = 0; i < as.size(); ++i) {
 		printArray(as[i]);
-		printArray(bs[i]);	
+		printArray(bs[i]);
 		std::vector<int> newA = as[i];
-		newA.resize(a.size() + b.size());
+	//	newA.reserve(as[i].size() + bs[i].size()); // Not useful; writes to locations beyond a.size() - 1 are ignored.
 		mergeSortedArrays(newA, bs[i]);
-		std::cout << "After: " << printArray(newA) << '\n'; 
+		std::cout << "After: ";
+		printArray(newA);
+		std::cout << '\n';
 	}
 }
 
