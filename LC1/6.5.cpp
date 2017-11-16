e9a7a1c3f8b17117d900aacc80873a92a1a27fdf
jagrut08 <jagrut_dave@hotmail.com>
2017-11-16 17:32:17 -0500

    Create 6.5.cpp

---
+++ LC1/6.5.cpp
@@ -0,0 +1,20 @@
+add reverse sorted to special cases for I
+--
+void sortSLL(lnPtr& head)
+--
+null
+ascending
+dups
+already sorted
+reverse sorted
+empty
+{1}
+{2, 1}
+{1, 2}
+{1, 2, 2}
+{2, 2, 5, 1, 3}
+{5, 1, 3, 2, 2}
+{4, 2, 3, 5}
+--
+BF
+	
