60cb7eedec6388c98ba3603cc9b1478a34d0d4fa
jagrut08 <jagrut_dave@hotmail.com>
2017-12-20 17:44:20 -0500

    Create 7.2.cpp

---
+++ LC1/7.2.cpp
@@ -0,0 +1,21 @@
+// O(logN) time
+int findIndex(const std::vector<int>& v, const int val) {
+	if(v.empty()) {
+		return 0;
+	}
+	int low = 0, high = v.size() - 1, mid = 0;
+
+	while(low <= high) { // The default for BS is <=
+		mid = low + (high - low) / 2;
+		if(val == v[mid]) {
+			return mid;
+		} else if(val > v[mid]) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+
+	return low; // Simpler than below
+//	return val > v[mid] ? mid + 1 : mid;
+}
